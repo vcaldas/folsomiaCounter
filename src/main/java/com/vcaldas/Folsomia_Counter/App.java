7fc59f1702ae2e545ee32521011c3067497f7949
Victor Caldas <caldas.victor@gmail.com>
2017-03-06 10:22:54 +0100

    first commit

---
+++ src/main/java/com/vcaldas/Folsomia_Counter/App.java
@@ -0,0 +1,13 @@
+package com.vcaldas.Folsomia_Counter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
